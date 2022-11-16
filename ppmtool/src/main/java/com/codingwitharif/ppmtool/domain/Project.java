aa56fa29dc231cf1b3d79e11490cb2e606e01856
unknown <arifcse03@gmail.com>
2022-11-16 15:23:05 +0600

    validation added on controller

--- ppmtool/src/main/java/com/codingwitharif/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/codingwitharif/ppmtool/domain/Project.java
@@ -1,10 +1,15 @@
 package com.codingwitharif.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.sql.Update;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -15,13 +20,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
-
+    @NotBlank(message = "Project Name is required")
     private  String projectName;
+    @NotBlank(message = "project Identifier  is required")
+    @Size(min = 4,max = 5,message = "Please use 4-5 characters")
+    @Column(updatable = false,unique = true)
     private String projectIdentifier;
+
     private  String description;
+
+    @JsonFormat(pattern = "dd-mm-yyyy")
     private Date start_date;
+    @JsonFormat(pattern = "dd-mm-yyyy")
     private Date end_date;
+    @JsonFormat(pattern = "dd-mm-yyyy")
     private  Date created_At;
+    @JsonFormat(pattern = "dd-mm-yyyy")
     private  Date updated_At;
     @PrePersist
     protected  void onCreate(){

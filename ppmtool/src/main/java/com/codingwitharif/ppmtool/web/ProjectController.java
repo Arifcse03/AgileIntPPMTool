1f3d1e7eaf7c44f8a305f6cca71c47e06f66e87a
unknown <arifcse03@gmail.com>
2022-11-17 14:18:46 +0600

    find All option added

--- ppmtool/src/main/java/com/codingwitharif/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/codingwitharif/ppmtool/web/ProjectController.java
@@ -8,11 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.security.Principal;
+import java.util.List;
 import javax.validation.Valid;
 
 
@@ -30,13 +29,19 @@ public class ProjectController {
 
 
     @PostMapping("")
-    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result, Principal principal){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if(errorMap!=null) return errorMap;
 
-        Project project1 = projectService.saveOrUpdateProject(project, principal.getName());
+        Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
-
+    @GetMapping("/{projectId}")
+public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+        Project project = projectService.findProjectByIdentifier(projectId);
+        return new ResponseEntity<Project>(project,HttpStatus.OK);
+    }
+    @GetMapping("/all")
+    public List<Project>getAllProjects(){return  projectService.findAllProjects();}
 }

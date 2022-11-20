baec6f18aceccefe0a7ff6a380ac70733c05d692
unknown <arifcse03@gmail.com>
2022-11-20 14:40:22 +0600

    dash board addded

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,28 +1,18 @@
 import logo from './logo.svg';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './App.css';
+import Dashboard from './Component/Dashboard';
+import Header from './Component/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit < code > src / App.js < /code> arif done. <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> <
-        /header> <
-        /div>
-    );
+  return (
+    <div className="App">
+    <Header/>
+      <Dashboard/>
+    </div>
+  );
 }
 
 export default App;
